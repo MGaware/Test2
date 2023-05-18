6998693b217a0114a6caac0d7e22b3ad889175ea
hp <hp@DESKTOP-T43P9M7>
2023-05-18 08:58:52 +0530

    Add to index...

--- src/Package1/Class1.java
+++ src/Package1/Class1.java
@@ -1,23 +1,5 @@
 package Package1;
 
-import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.datatransfer.StringSelection;
-import java.awt.event.KeyEvent;
-import java.util.List;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.Color;
-import org.openqa.selenium.support.ui.Select;
-
 public class Class1 
 {
 	
